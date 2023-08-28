ce849dc559ea8511df2aef2e07cb0d318b618e39
hugopukito <gogo26hugop@gmail.com>
2023-08-28 22:04:10 +0200

    1st version finish

--- app.js
+++ app.js
@@ -26,3 +26,29 @@ function scrollToDiv(id) {
   document.body.style.overflow = "visible"
   document.documentElement.style.overflow = "visible"
 }
+
+function openModal() {
+  toggleNavBar()
+
+  var modalDiv = document.getElementById('modal-background')
+  modalDiv.classList.add('show')
+
+  var menuButton = document.getElementById('menu-button')
+  menuButton.style.cursor = 'auto';
+  menuButton.ontouchstart = null
+
+  document.body.style.overflow = "hidden"
+  document.documentElement.style.overflow = "hidden"
+}
+
+function closeModal() {
+  var modalDiv = document.getElementById('modal-background')
+  modalDiv.classList.remove('show')
+
+  var menuButton = document.getElementById('menu-button')
+  menuButton.style.cursor = 'pointer';
+  menuButton.ontouchstart = toggleNavBar;
+
+  document.body.style.overflow = "visible"
+  document.documentElement.style.overflow = "visible"
+}
